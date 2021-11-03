8b3a5f267dbb27a05087950e3e100940fb706117
Tsyganno <tsyganlee@gmail.com>
2021-11-03 22:15:44 +0800

    first commit

---
+++ class_transport.py
@@ -0,0 +1,51 @@
+class Transport:
+    def __init__(self, brand, max_speed, kind=None):
+        self.brand = brand
+        self.max_speed = max_speed
+        self.kind = kind
+
+    def __str__(self):
+        return f'Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч'
+
+
+class Car(Transport):
+    def __init__(self, brand, max_speed, mileage, gasoline_residue):
+        super().__init__(brand, max_speed, kind='Car')
+        self.mileage = mileage
+        self.__gasoline_residue = gasoline_residue
+
+    @property
+    def gasoline(self):
+        return self.__gasoline_residue
+
+    @gasoline.getter
+    def gasoline(self):
+        return f'Осталось бензина на {self.__gasoline_residue} км'
+
+    @gasoline.setter
+    def gasoline(self, value):
+        if type(value) == int:
+            self.__gasoline_residue += value
+            print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
+        else:
+            print(f'Ошибка заправки автомобиля')
+
+
+class Boat(Transport):
+    def __init__(self, brand, max_speed, owners_name):
+        super().__init__(brand, max_speed, kind='Boat')
+        self.owners_name = owners_name
+
+    def __str__(self):
+        return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
+
+
+class Plane(Transport):
+    def __init__(self, brand, max_speed, capacity):
+        super().__init__(brand, max_speed, kind='Plane')
+        self.capacity = capacity
+
+    def __str__(self):
+        return f'Самолет марки {self.brand} вмещает в себя {self.capacity} людей'
+
+    
